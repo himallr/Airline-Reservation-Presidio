3962271eadd6d0ad3308877b815a6a2860df455d
HIMAL L R <117270050+himallr@users.noreply.github.com>
2024-01-09 21:17:42 +0530

    Update BookFlights.js

--- airline_reserv/src/BookFlights.js
+++ airline_reserv/src/BookFlights.js
@@ -1,121 +1,101 @@
-import React, { useState } from 'react'
-import { getFlightName } from './ApiHelpers';
+import React, { useState } from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { filterSearch } from './ApiHelpers';
+import BookFlight from './BookFlight';
 
 const BookFlights = () => {
-    const [selectedOption, setSelectedOption] = useState(null);
-    const [booknames, setBookNames] = useState([]);
-    const [name, setName] = useState('');
+    const [flights, setFlight] = useState([]);
+    const [formData, setFormData] = useState({
+        source: '',
+        destination: '',
+        flightClass: 'Economy Class'
+    });
 
-    const handleOptionSelect = (option) => {
-        setSelectedOption(option);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
     };
 
-    const handleNameChange = (event) => {
-        const names = event.target.value
-        setName(event.target.value);
-        getFlightName(names).then((data) => { setBookNames(data.books) })
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        filterSearch(formData.flightClass, formData.source, formData.destination)
+            .then((data) => {
+                console.log(data);
+                setFlight(data);
+            })
+        console.log(formData);
     };
 
-    const options = ["Madurai", "Bengaluru", "Kolkata", "Chennai", "Pune", "Rajasthan", "Trichy"];
+    const places = ["Mumbai", "Chennai", "Delhi", "Kolkata", "Bangalore", "Pune", "Hyderabad", "Virudhunagar"]
 
+    console.log(places);
     return (
-        <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-md-3'>
-                    <div className="row">
-                        <div className="col-md-6 mx-auto">
-                            <div className='my-3'>
-                                <input
-                                    type="text"
-                                    id="genre"
-                                    value={name}
-                                    onChange={handleNameChange}
-                                    className='form-control border-3 border-success'
-                                    placeholder="Search..."
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className='col-md-2'>
-                    <div className='bg-secondary'>
-                        <div className=''>Source</div>
-                        <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {selectedOption}
-                        </button>
-                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            {options.map((option, index) => (
-                                <button
-                                    key={index}
-                                    className="dropdown-item"
-                                    onClick={() => handleOptionSelect(option)}
-                                >
-                                    {option}
-                                </button>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-                <div className='col-md-2'>
-                    <div className='bg-secondary'>
-                        <div className=''>Destination</div>
-                        <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {selectedOption}
-                        </button>
-                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            {options.map((option, index) => (
-                                <button
-                                    key={index}
-                                    className="dropdown-item"
-                                    onClick={() => handleOptionSelect(option)}
-                                >
-                                    {option}
-                                </button>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-                <div className='col-md-2'>
-                    <div className='bg-secondary'>
-                        <div className=''>Timing</div>
-                        <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {selectedOption}
-                        </button>
-                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            {options.map((option, index) => (
-                                <button
-                                    key={index}
-                                    className="dropdown-item"
-                                    onClick={() => handleOptionSelect(option)}
-                                >
-                                    {option}
-                                </button>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-                <div className='col-md-2'>
-                    <div className='bg-secondary'>
-                        <div className=''>Source</div>
-                        <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {selectedOption}
-                        </button>
-                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            {options.map((option, index) => (
-                                <button
-                                    key={index}
-                                    className="dropdown-item"
-                                    onClick={() => handleOptionSelect(option)}
-                                >
-                                    {option}
-                                </button>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+        <Container>
+            <Row className="justify-content-center mt-5">
+                <Col md={6}>
+                    <Form onSubmit={handleSubmit}>
+                        <h2 className='text-center text-primary text-bold'>Book Details</h2>
+                        <Form.Group controlId="source">
+                            <Form.Label>Source</Form.Label>
+                            <Form.Select
+                                type="text"
+                                name="source"
+                                value={formData.source}
+                                onChange={handleChange}
+                                required
+                            >
+                                {
+                                    places && places.map((data) => {
+                                        return <option value={data}>{data}</option>
+                                    })
+                                }
+                            </Form.Select>
+                        </Form.Group>
 
-export default BookFlights
+                        <Form.Group controlId="destination">
+                            <Form.Label>Destination</Form.Label>
+                            <Form.Select
+                                type="text"
+                                name="destination"
+                                value={formData.destination}
+                                onChange={handleChange}
+                                required
+                            >
+                                {
+                                    places && places.map((data) => {
+                                        return <option value={data}>{data}</option>
+                                    })
+                                }
+                            </Form.Select>
+                        </Form.Group>
+
+                        <Form.Group controlId="flightClass">
+                            <Form.Label>Class</Form.Label>
+                            <Form.Select
+                                name="flightClass"
+                                value={formData.flightClass}
+                                onChange={handleChange}
+                            >
+                                <option value="Economy Class">Economy Class</option>
+                                <option value="Business Class">Business Class</option>
+                                <option value="Business Premium Class">Business Premium Class</option>
+                            </Form.Select>
+                        </Form.Group>
+
+                        <div className="d-flex justify-content-center">
+                            <Button className='mt-3 text-center' variant="primary" type="submit">
+                                View Flights
+                            </Button>
+                        </div>
+                    </Form>
+                </Col>
+            </Row>
+            {
+                flights &&
+                <BookFlight flights={flights} />
+            }
+        </Container>
+    );
+};
+
+export default BookFlights;
